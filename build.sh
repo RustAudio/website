86d9f7170d92a38d23aab9df598bea50d7df7e82
doomy <alexanderpaullozada@gmail.com>
2019-04-08 18:22:04 -0500

    i hate ci

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 ZOLA_VERSION="0.6.0"
 curl -sL https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz | tar zxv
 ./zola build
